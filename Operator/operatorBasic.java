23d59843e43e263fe82f20a5bed49c0844eb7867
Ashish Kumar Yadav <ashishkumaryadavcse507@gmail.com>
2024-04-07 09:48:10 +0530

    Day 2

---
+++ Operator/operatorBasic.java
@@ -0,0 +1,39 @@
+package Operator;
+
+public class operatorBasic {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        // Airthmetic Operator
+
+        // System.out.println("Add :" + (a + b));
+        // System.out.println("Subtract :" + (a - b));
+        // System.out.println("Multiplication :" + (a * b));
+        // System.out.println("Divide :" + (a / b));
+        // System.out.println("Modulo :" + (a % b));
+
+        // System.out.println("Post Increment :" + (a++));
+        // System.out.println("Pre Increment :" + (++a));
+
+        // System.out.println("Post Decrement :" + (a--));
+        // System.out.println("Pre Decrement :" + (--a));
+
+        // Relational Operator
+
+        // System.out.println("Equal :" + (a == b));
+        // System.out.println("Not Equal :" + (a != b));
+        // System.out.println("Greater Then :" + (a > b));
+        // System.out.println("Less Then :" + (a < b));
+        // System.out.println("Greater Then Equal :" + (a >= b));
+        // System.out.println("Less Then Equal :" + (a <= b));
+
+        // Logical Operator
+
+        // System.out.println("And :" + ((a > b) && (b < a)));
+        // System.out.println("OR :" + ((a < b) || (b == a)));
+        // System.out.println("NOT :" + (!(a > b)));
+
+    }
+
+}
